1497c52197ceafbf8ff2da576d18287e23bdb21c
Matthias Bilger <matthias@bilger.info>
2019-12-10 17:07:04 +0100

    round did the trick

--- day10/module.py
+++ day10/module.py
@@ -13,25 +13,22 @@ inp = get_input(inp_decode)
 _logger = get_logger()
 
 def visible(my_x,my_y,inp):
+    # Use a set to store the factor and the upper or lower half of the coordinate system
     rs = set()
     if inp[my_y][my_x] != '#':
         return 0
     for x in range(0, len(inp[0])):
         for y in range(0, len(inp)):
             if inp[y][x] == '#':
-                if x == my_x:
-                    if y == my_y:
-                        continue
-                    n = '-' if y<my_y else '+'
+                n = '-' if y<my_y else '+'
+                if x == my_x and y == my_y:
+                    continue
+                elif x == my_x:
                     rs.add(f'x{n}')
                 elif y == my_y:
-                    if x == my_x:
-                        continue
-                    n = '-' if x<my_x else '+'
                     rs.add(f'y{n}')
                 else:
                     res = (1.0*y - my_y)/(1.0*x - my_x)
-                    n = '-' if y<my_y else '+'
                     rs.add(f'{n}{res}')
     return len(rs)
 
@@ -58,29 +55,16 @@ count = 0
 result = None
 def destroy(my_x,my_y,minp):
     inp = minp.copy()
-    coords2 = []
     inp[my_y][my_x] = '0'
-    coords = defaultdict(lambda: defaultdict(lambda: 0))
+    coords = defaultdict(lambda: {})
     for x in range(0, len(inp[0])):
         for y in range(0, len(inp)):
             if inp[y][x] == '#':
                 thex = x-my_x
                 they = y-my_y
                 r = math.sqrt(thex*thex +  they*they)
-                if r == 0: 
+                if r == 0:
                     continue
-                if False:
-                    if thex > 0:
-                        phi = math.atan(they/thex)
-                    if thex < 0 and they >= 0:
-                        phi = math.atan(they/thex) + math.pi
-                    if thex < 0 and they < 0:
-                        phi = math.atan(they/thex) - math.pi
-                    if thex == 0 and they > 0:
-                        phi = math.pi/2
-                    if thex == 0 and they < 0:
-                        phi = -math.pi/2
-                    phi = phi+math.pi
                 if they >= 0:
                     phi = math.acos(thex/r)
                 if they < 0:
@@ -90,9 +74,8 @@ def destroy(my_x,my_y,minp):
                     phi = phi - math.pi *2
                 if phi < 0:
                     phi = phi + math.pi *2
-                coords2.append((phi, r, (x,y)))
+                phi = round(phi, 8)
                 coords[phi][r] = (x,y)
-    c2 = sorted(coords2, key= lambda x: x[1])
     angles = sorted(coords.keys())
     dists = set()
     for i in coords.values():
@@ -101,6 +84,7 @@ def destroy(my_x,my_y,minp):
     dists = sorted(dists)
     result = None
     results = []
+    print_grid(inp)
     while result is None:
         for a in angles:
             def inner():
@@ -108,19 +92,21 @@ def destroy(my_x,my_y,minp):
                 for d in dists:
                     if d in coords[a]:
                         count += 1
-                        if count in [1, 2, 3, 10, 20, 50, 100, 199]:
+                        if count in [1, 2, 3, 10, 20, 50, 100, 199, 200]:
                             results.append(coords[a][d])
                         if count == 200:
                             result = coords[a][d]
                             inp[coords[a][d][1]][coords[a][d][0]] = 'C'
                             print_grid(inp)
+                            print(sum(map(len, coords.values())), count)
                             print(results)
                             return result
                         inp[coords[a][d][1]][coords[a][d][0]] = 'x'
                         print_grid(inp)
+                        print(sum(map(len, coords.values())), count)
                         print(results)
                         del coords[a][d]
-                        time.sleep(0.125)
+                        time.sleep(0.01525)
                         return
             result = inner()
             if result is not None:
@@ -128,9 +114,8 @@ def destroy(my_x,my_y,minp):
     return 0
 
 
-maxp = (0,0)
 def p1():
-    global maxp
+    maxp = (0,0)
     results = {}
     m = 0
     for y, row in enumerate(inp):
@@ -139,16 +124,15 @@ def p1():
             m = max (m, results[(x,y)])
             if m == results[(x,y)]:
                 maxp = (x,y)
-                print('----------', x, y)
-    return m
+    return m, maxp
 
 def p2(result):
     return destroy(*result,inp)
     return None
 
 def main():
+    result, maxp = p1()
     if part_one():
-        result = p1()
         print(f'Result: {result}')
         print(maxp)
 

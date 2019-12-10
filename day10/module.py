f6fb9b6f48716d0bc6178c237045d091f3388fe7
Matthias Bilger <matthias@bilger.info>
2019-12-10 06:19:45 +0100

    write detected

--- day10/module.py
+++ day10/module.py
@@ -31,7 +31,8 @@ def visible(my_x,my_y,inp):
         for cur_x in range(0, len(inp[1])):
             cur_y = cur_x * r
             if cur_y.is_integer():
-                rs[r] = inp[int(cur_x)][cur_x] == '#'
+                if inp[int(cur_x)][cur_x] == '#':
+                    rs[r] = 1
                 break
                 print(cur_x, int(cur_y))
     print(len(rs))

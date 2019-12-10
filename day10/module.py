38c4c4438ddcb0e11fab40360db9e95fa0979fd3
Matthias Bilger <matthias@bilger.info>
2019-12-10 07:29:07 +0100

    tey2

--- day10/module.py
+++ day10/module.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from collections import defaultdict
 from aoc.input import get_input
 from aoc.partselector import part_one, part_two, get_logger
@@ -10,42 +11,75 @@ inp = get_input(inp_decode)
 _logger = get_logger()
 
 def visible(my_x,my_y,inp):
-    checkpoints = []
+    rs = set()
+    if inp[my_y][my_x] != '#':
+        return 0
     for x in range(0, len(inp[0])):
-        checkpoints.append((x, 0))
-        checkpoints.append((x, len(inp)-1))
-    for y in range(0, len(inp)):
-        checkpoints.append((0, y))
-        checkpoints.append((len(inp[0])-1, y))
-    if (my_x, my_y) in checkpoints:
-        checkpoints.remove((my_x, my_y))
-
-    rs = {}
-    for cp in checkpoints:
-        count = 0
-        if (cp[0] - my_x) == 0:
-            continue
-        r = (1.0*cp[1] - my_y)/(cp[0] - my_x)
-        if r in rs:
-            continue
-        for cur_x in range(0, len(inp[1])):
-            cur_y = cur_x * r
-            if cur_y.is_integer() and cur_y > 0 and cur_y < len(inp):
-                if inp[int(cur_y)][cur_x] == '#':
-                    rs[r] = 1
-                    break
-
+        for y in range(0, len(inp)):
+            if inp[y][x] == '#':
+                if x == my_x:
+                    if y == my_y:
+                        continue
+                    n = '-' if y<my_y else '+'
+                    rs.add(f'x{n}')
+                elif y == my_y:
+                    if x == my_x:
+                        continue
+                    n = '-' if x<my_x else '+'
+                    rs.add(f'y{n}')
+                else:
+                    res = (1.0*y - my_y)/(1.0*x - my_x)
+                    n = '-' if y<my_y else '+'
+                    rs.add(f'{n}{res}')
     print(my_x,my_y,len(rs))
+    return len(rs)
+
+def destroy(my_x,my_y,minp):
+    inp = minp.copy()
+    coords2 = []
+    coords = defaultdict(lambda: defaultdict(lambda: 0))
+    for x in range(0, len(inp[0])):
+        for y in range(0, len(inp)):
+            if inp[y][x] == '#':
+                thex = x-my_x
+                they = y-my_y
+                r = math.sqrt(thex*thex +  they*they)
+                if r == 0: 
+                    continue
+                if thex > 0:
+                    phi = math.atan(they/thex)
+                if thex < 0 and they >= 0:
+                    phi = math.atan(they/thex) + math.pi
+                if thex < 0 and they < 0:
+                    phi = math.atan(they/thex) - math.pi
+                if thex == 0 and they > 0:
+                    phi = math.pi/2
+                if thex == 0 and they < 0:
+                    phi = -math.pi/2
+                phi = (phi+math.pi+math.pi/2)%(2*math.pi) - math.pi
+                coords2.append((phi, r, (x,y)))
+                coords[phi][r] = (x,y)
+    c2 = sorted(coords2, key= lambda x: x[0], reverse=True)
+    print(c2)
+    for i in range(0, 200):
+        c2
+    return c2[199][2]
+    return len(coords)
 
 
 def p1():
     results = {}
+    m = 0
     for y, row in enumerate(inp):
         for x, col in enumerate(row):
             results[(x,y)] = visible(x,y,inp)
-    return None
+            m = max (m, results[(x,y)])
+            if m == results[(x,y)]:
+                print('----------', x, y)
+    return m
 
 def p2():
+    return destroy(20,20,inp)
     return None
 
 def main():

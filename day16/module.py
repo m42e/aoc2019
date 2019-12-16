b5b0a5c29bf468b6d0b7b330bbbed553c00f0100
Matthias Bilger <matthias@bilger.info>
2019-12-16 08:43:54 +0100

    day16

--- day16/module.py
+++ day16/module.py
@@ -1,19 +1,50 @@
 import sys
 from collections import defaultdict
-from aoc.input import get_input
 from aoc.partselector import part_one, part_two, get_logger
+import aoc
 
 def inp_decode(x):
-    return list(map(int,x.split(',')))
+    return str(x)
 
-inp = get_input(inp_decode)
+inp = aoc.get_input(inp_decode)
 _logger = get_logger()
 
+
+def mult(inpt, pattern):
+    inpt = list(inpt)
+    absout = []
+    for n in range(1, len(inpt)+1):
+        indx = 1
+        outp = {}
+        rpattern = []
+        for p in pattern:
+            for i in range(0, n):
+                rpattern.append(p)
+
+        for z in range(0, len(inpt)):
+            res = int(inpt[z])*rpattern[indx]
+            outp[z] = res
+            indx = (indx+1)%len(rpattern)
+        absout.append(abs(sum(outp.values()))%10)
+    return absout
+
 def p1():
+    i = inp
+    print (inp)
+    for x in range(0, 100):
+         i = mult(i, [0, 1,0,-1])
+         print(''.join(map(str, i)))
     return None
 
 def p2():
-    return None
+    i = 10000*inp
+    print(int(i[0:7]))
+    ii = i[int(i[0:7]):]
+    new = list(map(int, ii))
+    for x in range(100):
+        for x in range(len(new)-2, -1, -1):
+            new[x] = (new[x] + new[x+1]) % 10
+    return ''.join(map(str, new[0:8]))
 
 def main():
     if part_one():
